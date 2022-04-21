9ad1cec33fbfe030966bba80607242b044fd6651
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-04-21 23:37:34 +0900

    [update] jpa repository

--- src/main/java/com/cloudlibrary/auth/infrastructure/persistence/mysql/repository/AuthEntityRepository.java
+++ src/main/java/com/cloudlibrary/auth/infrastructure/persistence/mysql/repository/AuthEntityRepository.java
@@ -1,11 +1,12 @@
 package com.cloudlibrary.auth.infrastructure.persistence.mysql.repository;
 
 import com.cloudlibrary.auth.application.domain.Auth;
+import com.cloudlibrary.auth.infrastructure.persistence.mysql.entity.AuthEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface AuthEntityRepository {
-    Optional<Auth> findAuthById(Long authId);
-    List<Auth> findAuthAll();
+public interface AuthEntityRepository extends JpaRepository<AuthEntity, Long> {
+
 }
