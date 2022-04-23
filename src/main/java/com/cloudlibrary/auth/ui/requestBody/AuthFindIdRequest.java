18ff00cda85f5f8b8cd39d78e3de1392ba3868c4
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-04-23 15:05:18 +0900

    [update] add validation

--- src/main/java/com/cloudlibrary/auth/ui/requestBody/AuthFindIdRequest.java
+++ src/main/java/com/cloudlibrary/auth/ui/requestBody/AuthFindIdRequest.java
@@ -5,10 +5,16 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 public class AuthFindIdRequest {
+
+    @Email(message = "올바른 이메일 주소를 입력해주세요")
+    @NotBlank(message = "이메일은 필수값입니다")
     private String email;
 }
