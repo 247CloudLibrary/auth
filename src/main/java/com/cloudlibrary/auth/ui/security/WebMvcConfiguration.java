def60e9e763dc0cf300d958b9f6e224bebbcf6bb
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-04-27 02:32:42 +0900

    [update] Configuration 어노테이션 추가

--- src/main/java/com/cloudlibrary/auth/ui/security/WebMvcConfiguration.java
+++ src/main/java/com/cloudlibrary/auth/ui/security/WebMvcConfiguration.java
@@ -1,10 +1,12 @@
 package com.cloudlibrary.auth.ui.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Autowired
