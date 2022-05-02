e9bec23b54d5b51f06d823ec03e25af95632bad6
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-05-02 18:45:51 +0900

    [update] password 수정용 request 추가

---
+++ src/main/java/com/cloudlibrary/auth/ui/requestBody/AuthUpdatePassword.java
@@ -0,0 +1,24 @@
+package com.cloudlibrary.auth.ui.requestBody;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class AuthUpdatePassword {
+
+
+    private String oldPassword;
+
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{10,16}", message = "비밀번호는 10~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @NotBlank(message = "비밀번호는 필수값입니다")
+    private String newPassword;
+
+}
